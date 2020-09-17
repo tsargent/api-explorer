f0c52a05ecb630d892d2f20e4ecda5bcefb422e2
Tyler Sargent <tsargent@gmail.com>
2020-09-16 22:30:15 -0400

    styling

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types'
 
 function Entry({entry}) {
   return (
-    <div className="shadow-md rounded p-4 bg-pink-100">
+    <div className="shadow-md rounded p-4 bg-pink-100 text-gray-900">
       <h2 className="text-2xl mb-2">{entry.API}</h2>
-      <p>{entry.Description}</p>
+      <p className="text-xl mb-2">{entry.Description}</p>
       <p>Category: {entry.Category}</p>
       <p>Auth: {entry.Auth || 'none'}</p>
       <p>HTTPS: {entry.HTTPS.toString()}</p>
-      <p>Cors: {entry.Cors}</p>
+      <p className="mb-2">Cors: {entry.Cors}</p>
       <p>
         <a className="text-blue-700" href={entry.Link}>
           {entry.Link}
