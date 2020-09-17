65c08f5bce28ec47799b2db1caeb98f8fe34e05a
Tyler Sargent <tsargent@gmail.com>
2020-09-16 23:24:49 -0400

    link home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {hot} from 'react-hot-loader'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Link} from 'react-router-dom'
 import {ReactQueryDevtools} from 'react-query-devtools'
 import Random from './components/Random'
 import List from './screens/List'
@@ -10,7 +10,9 @@ const App = () => {
   return (
     <div className="p-2 bg-teal-400 min-h-screen">
       <nav className="p-4">
-        <h1 className="text-2xl font-bold text-pink-100">API Explorer</h1>
+        <h1 className="text-2xl font-bold text-pink-100">
+          <Link to="/">API Explorer</Link>
+        </h1>
       </nav>
       <main className="px-4">
         <Routes>
